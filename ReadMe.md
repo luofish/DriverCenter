3e89ee8fcb9fa1e0de7d36382fa794d48295da18
luofish <luoz@macdeMacBook-Pro.local>
2016-03-21 19:01:54 +0800

    update

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+# project document
+
+## useful link
+- https://nodejs.org/en/docs/
+- https://code.visualstudio.com/docs/runtimes/nodejs
---
+++ core/base.js
@@ -0,0 +1,22 @@
+
+
+class BaseService{
+    
+    testMethod(){
+        
+        
+    }
+}
+ 
+
+
+class BaseRoute{
+    
+    
+}
+
+
+
+
+exports.BaseRoute = BaseRoute;
+exports.BaseService = BaseService;
